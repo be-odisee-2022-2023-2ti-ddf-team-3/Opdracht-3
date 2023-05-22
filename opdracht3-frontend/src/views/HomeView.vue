91591ff4f95096b887479997d093ca62e7caf7f0
ThomasRoelandts <85442431+Thomas-Roelandts@users.noreply.github.com>
2023-05-22 20:09:18 +0200

    frontend verder gewerkt

--- opdracht3-frontend/src/views/HomeView.vue
+++ opdracht3-frontend/src/views/HomeView.vue
@@ -1,9 +1,51 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>personen</h1>
+    <table>
+      <tr>
+        <th>id</th>
+        <th>naam</th>
+        <th>email</th>
+        <th>paswoord</th>
+        <th>rol</th>
+        <th>acties</th>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
   </main>
 </template>
+<script>
+export default {
+  props: {
+    msg: String
+  },
+
+  methods: {
+    fetchData() {
+      fetch('https://localhost/8080/personen', {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host': 'random-facts2.p.rapidapi.com',
+          'x-rapidapi-key': 'Your -RapidAPI-Hub-Key'
+        }
+      })
+        .then(response => {
+          response.json().then(res => console.log(res));
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
+  },
+  beforeMount() {
+    this.fetchData()
+  }
+};
+</script>
